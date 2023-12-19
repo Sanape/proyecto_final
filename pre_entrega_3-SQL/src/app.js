92161f2e45921e19858ebbe927528c48490dbad5
santiago <sanape.SA@GMAIL.com>
2023-12-18 23:53:02 -0300

    correccion de path global (debido a creacion de carpeta 'utils')

--- pre_entrega_3-SQL/src/app.js
+++ pre_entrega_3-SQL/src/app.js
@@ -1,31 +1,32 @@
 //Imports
-import "dotenv/config";
-import express from "express";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import productRoute from "./routes/product.routes.js";
-import cartRoute from "./routes/cart.routes.js";
-import sessionRoute from "./routes/session.routes.js";
-import userRoute from "./routes/user.routes.js";
-import ratingRoute from "./routes/rating.routes.js";
-import developerRoute from "./routes/developer.routes.js";
-import viewsRoute from "./routes/views.routes.js";
-import categoryRoute from "./routes/category.routes.js";
-import errorHandlerMiddleware from "./middlewares/error.middleware.js";
-import { engine } from "express-handlebars";
-import { __dirname } from "./utils/utils.js";
-import { Server } from "socket.io";
+import 'dotenv/config';
+import express from 'express';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import productRoute from './routes/product.routes.js';
+import cartRoute from './routes/cart.routes.js';
+import sessionRoute from './routes/session.routes.js';
+import userRoute from './routes/user.routes.js';
+import ratingRoute from './routes/rating.routes.js';
+import developerRoute from './routes/developer.routes.js';
+import viewsRoute from './routes/views.routes.js';
+import categoryRoute from './routes/category.routes.js';
+import errorHandlerMiddleware from './middlewares/error.middleware.js';
+import { engine } from 'express-handlebars';
+import { __dirname } from './utils/utils.js';
+import { Server } from 'socket.io';
 import {
   getAllProductsHandler,
   messagesHandler,
   getRandomBuy,
   getAllCategoriesHandler,
   getAllDevelopersHandler,
-} from "./handlers/handlers.js";
-import { Database } from "./config/database.connection.js";
-import passport from "passport";
+} from './handlers/handlers.js';
+import { Database } from './config/database.connection.js';
+import passport from 'passport';
+import path from 'path';
 
 //Variables
 const app = express();
@@ -33,12 +34,12 @@ const app = express();
 //Configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-app.use(morgan("dev"));
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.engine("handlebars", engine());
-app.set("views", __dirname + "/views");
-app.set("view engine", "handlebars");
+app.engine('handlebars', engine());
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'handlebars');
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
@@ -59,14 +60,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //Routes
-app.use("/api/carts", cartRoute);
-app.use("/api/products", productRoute);
-app.use("/api/sessions", sessionRoute);
-app.use("/api/users", userRoute);
-app.use("/api/ratings", ratingRoute);
-app.use("/api/categories", categoryRoute);
-app.use("/api/developers", developerRoute);
-app.use("/", viewsRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/products', productRoute);
+app.use('/api/sessions', sessionRoute);
+app.use('/api/users', userRoute);
+app.use('/api/ratings', ratingRoute);
+app.use('/api/categories', categoryRoute);
+app.use('/api/developers', developerRoute);
+app.use('/', viewsRoute);
 
 //Global middlewares
 app.use(errorHandlerMiddleware);
@@ -87,6 +88,6 @@ const onConnection = async (socket) => {
   await getAllDevelopersHandler(socketServer, socket);
 };
 
-socketServer.on("connection", onConnection);
+socketServer.on('connection', onConnection);
 
 export default app;
